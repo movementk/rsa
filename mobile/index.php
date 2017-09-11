598c8f1397f9df5d7ce63eceec9fcea40163dcb0
kimyh5392 <mk.kimyh@gmail.com>
2017-09-11 18:22:35 +0900

    모바일 메인 레이아웃 코딩 및 셋팅 작업 완료.

---
+++ mobile/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RS Automation</title>
+
+        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header id="header">
+            <h1>Header</h1>
+        </header>
+        <main id="content">
+            <h2>Content</h2>
+        </main>
+        <footer id="footer">
+            <h3>Footer</h3>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
