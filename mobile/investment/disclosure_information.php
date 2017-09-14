63077dfbe128fe34268c74e476053d7d84fc7dba
kimyh5392 <mk.kimyh@gmail.com>
2017-09-14 18:58:51 +0900

    customer 작업중. . .

---
+++ mobile/investment/disclosure_information.php
@@ -0,0 +1,132 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/assets/css/sub.css" rel="stylesheet">
+<link href="/mobile/assets/css/investment.css" rel="stylesheet">
+</head>
+<body class="investment">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <!-- Content Start -->
+    <main id="content">
+        <!-- page-header -->
+        <div class="page-header">
+            <div class="container">
+                <h2>공시정보</h2>
+            </div>
+            <div class="btn-control">
+                <a href="/mobile/investment/stock_information.php" class="btn-pre">
+                    <i class="icon-left-open-big">
+                        <span class="sr-only">이전 페이지</span>
+                    </i>
+                </a>
+                <a href="/mobile/investment/finance_information.php" class="btn-next">
+                    <i class="icon-right-open-big">
+                        <span class="sr-only">다음 페이지</span>
+                    </i>
+                </a>
+            </div>
+        </div>
+        
+        <!-- page content -->
+        <section class="disclosure-information">
+            <div class="container">
+                <div class="section-header">
+                    <h3 class="section-title">공시정보</h3>
+                </div>
+                <div class="section-content">
+                    <ul class="board-list">
+                        <li>
+                            <a href="#">
+                                <p class="subject">[정정] 분기보고서 (일반법인)분기보고서 (일반법인)분기보고서 (일반법인)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">전환청구권행사(2회차)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">추가상장(국내사모 CB전환)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">전환청구권행사(1회차)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">전환청구권행사(1회차)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">추가상장(국내사모 CB전환)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">전환청구권행사(1회차)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p class="subject">전환청구권행사(1회차)</p>
+                                <dl>
+                                    <dt>등록일</dt>
+                                    <dd>2017-08-05</dd>
+                                    <dt>제출의무자</dt>
+                                    <dd>RSA</dd>
+                                </dl>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+</body>
+</html>
